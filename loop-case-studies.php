9d10a0e3a5ccde44099b149662e4a59c372c3a42
Josh Carey <josh@harbr.co>
2016-01-26 15:09:45 -0500

    convert case study archive query to wp_query

--- loop-case-studies.php
+++ loop-case-studies.php
@@ -20,33 +20,44 @@
    $rgb = hex2rgb($brandColor);
 ?>
 
-<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php
+$args = array(
+   'post_type' => 'case-studies',
+   'orderby' => 'menu_order'
+);
+$caseStudiesQuery = new WP_Query( $args ); ?>
+<?php if ( $caseStudiesQuery->have_posts() ) : ?>
+   <!-- the loop -->
+   <?php while ( $caseStudiesQuery->have_posts() ) : $caseStudiesQuery->the_post(); ?>
 
-   <?php $brandColor = get_field('brand_color'); ?>
+      <?php $brandColor = get_field('brand_color'); ?>
 
-   <?php
-      global $post;
-      $post_slug=$post->post_name;
-   ?>
+      <?php
+         global $post;
+         $post_slug=$post->post_name;
+      ?>
 
-   <article class="section post-hero hero view-post-cta big-cta isDarkGray <?php echo $post_slug; ?>" style="background-color: #<?php echo $brandColor; ?>;">
-      <div class="section-heading">
-         <h3 class="clrPop">Case Study</h3>
-         <span class="divWave"></span>
-         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-         <a href="<?php the_permalink(); ?>" class="btn btn--uline">View Case Study</a>
-      </div>
+      <article class="section post-hero hero view-post-cta big-cta isDarkGray <?php echo $post_slug; ?>" style="background-color: #<?php echo $brandColor; ?>;">
+         <div class="section-heading">
+            <h3 class="clrPop">Case Study</h3>
+            <span class="divWave"></span>
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            <a href="<?php the_permalink(); ?>" class="btn btn--uline">View Case Study</a>
+         </div>
 
-      <div class="hero-highlight-image" style="background-image: url(<?php the_field('hero_highlight_image') ?>);">
-      </div>
+         <div class="hero-highlight-image" style="background-image: url(<?php the_field('hero_highlight_image') ?>);">
+         </div>
 
-      <!-- Scroll Down Button -->
-      <a href="#" class="downArrow next-post-section"></a>
-   </article>
+         <!-- Scroll Down Button -->
+         <a href="#" class="downArrow next-post-section"></a>
+      </article>
 
-<?php endwhile; ?>
+   <?php endwhile; ?>
+   <!-- end of the loop -->
 
    <?php get_template_part('pagination'); ?>
+
+   <?php wp_reset_postdata(); ?>
 
 <?php else: ?>
 <?php endif; ?>
